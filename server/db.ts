c865cbc7c837d3cc6f474092cbab300a46a79ced
BSenichak <bsenk77@gmail.com>
2025-12-02 19:19:23 +0200

    fix: set default values for database connection parameters

--- server/db.ts
+++ server/db.ts
@@ -6,9 +6,10 @@ config();
 const DB_NAME = process.env.DB_NAME!;
 
 const rootPool = createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "root",
+    port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
     waitForConnections: true,
     connectionLimit: 10,
 });
