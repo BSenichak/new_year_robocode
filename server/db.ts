420415b252f5f1e4c1da30bbe8e8fd0fddbe37c8
BSenichak <bsenk77@gmail.com>
2025-12-02 19:45:08 +0200

    fix

--- server/db.ts
+++ server/db.ts
@@ -9,7 +9,6 @@ const rootPool = createPool({
     host: process.env.DB_HOST || process.env.MYSQLHOST,
     user: process.env.DB_USER || process.env.MYSQLUSER,
     password: process.env.DB_PASSWORD || process.env.MYSQLPASSWORD,
-    database: process.env.DB_NAME || process.env.MYSQLDATABASE,
     port: Number(process.env.DB_PORT || process.env.MYSQLPORT || 3306),
     waitForConnections: true,
     connectionLimit: 10,
