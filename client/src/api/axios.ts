655731e1af0ef040e14b8c717ab902099124c1ca
BSenichak <bsenk77@gmail.com>
2025-12-04 10:42:31 +0200

    Remove unused HTML and SVG files from the server distribution

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -2,9 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-  baseURL: import.meta.env.DEV
-    ? "http://localhost:5000/api"
-    : "api",
+  baseURL: import.meta.env.VITE_RAILWAY_PUBLIC_DOMAIN + "/api",
   withCredentials: true,
   timeout: 10000,
 });
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
-
-// https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-
-    build: {
-        outDir: path.resolve(__dirname, "../server/dist"),
-        emptyOutDir: true,
-    },
 });
